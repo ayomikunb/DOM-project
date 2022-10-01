30b550f3686b6003b8e77ccd70de6955b2796756
ayomikunb <ayo.bakare199@gmail.com>
2022-10-01 09:58:36 +0100

    DOM project

---
+++ script.js
@@ -0,0 +1,158 @@
+
+   var i=0;
+
+ function incNumber() {
+    if (i < 100) {
+        i++;
+     } else if (i = 100) {
+        i = 0;
+      }
+      document.querySelector("#display").innerHTML= i
+      
+  }
+ 
+  function decNumber() {
+      if (i > 0) {
+          --i;
+      } else if (i = 0) {
+          i = 10;
+     }
+      document.querySelector("#display").innerHTML= i
+     
+  }
+ 
+  function changeColor()
+{
+   var icon = document.querySelector('#heart');
+   icon.style.color = "red";    
+}
+
+
+
+
+function incNumber2() {
+   if (i < 100) {
+       i++;
+    } else if (i = 100) {
+       i = 0;
+     }
+     document.querySelector("#display2").innerHTML= i;
+ }
+
+ function decNumber2() {
+     if (i > 0) {
+         --i;
+     } else if (i = 0) {
+         i = 10;
+    }
+     document.querySelector("#display2").innerHTML = i;
+ }
+
+ function changeColor2()
+{
+  var icon = document.querySelector('#heart2');
+  icon.style.color = "red";    
+}
+
+
+
+function incNumber3() {
+    if (i < 100) {
+        i++;
+     } else if (i = 100) {
+        i = 0;
+      }
+      document.querySelector("#display3").innerHTML= i;
+  }
+ 
+  function decNumber3() {
+      if (i > 0) {
+          --i;
+      } else if (i = 0) {
+          i = 10;
+     }
+      document.querySelector("#display3").innerHTML = i;
+  }
+ 
+  function changeColor3()
+ {
+   var icon = document.querySelector('#heart3');
+   icon.style.color = "red";    
+ }
+ 
+ 
+function incNumber4() {
+    if (i < 100) {
+        i++;
+     } else if (i = 100) {
+        i = 0;
+      }
+      document.querySelector("#display4").innerHTML= i;
+  }
+ 
+  function decNumber4() {
+      if (i > 0) {
+          --i;
+      } else if (i = 0) {
+          i = 10;
+     }
+      document.querySelector("#display4").innerHTML = i;
+  }
+ 
+  function changeColor4()
+ {
+   var icon = document.querySelector('#heart4');
+   icon.style.color = "red";    
+ }
+ 
+ 
+function incNumber5() {
+    if (i < 100) {
+        i++;
+     } else if (i = 100) {
+        i = 0;
+      }
+      document.querySelector("#display5").innerHTML= i;
+  }
+ 
+  function decNumber5() {
+      if (i > 0) {
+          --i;
+      } else if (i = 0) {
+          i = 10;
+     }
+      document.querySelector("#display5").innerHTML = i;
+  }
+ 
+  function changeColor5()
+ {
+   var icon = document.querySelector('#heart5');
+   icon.style.color = "red";    
+ }
+
+
+ 
+function incNumber6() {
+    if (i < 100) {
+        i++;
+     } else if (i = 100) {
+        i = 0;
+      }
+      document.querySelector("#display6").innerHTML= i;
+  }
+ 
+  function decNumber6() {
+      if (i > 0) {
+          --i;
+      } else if (i = 0) {
+          i = 10;
+     }
+      document.querySelector("#display6").innerHTML = i;
+  }
+ 
+  function changeColor6()
+ {
+   var icon = document.querySelector('#heart6');
+   icon.style.color = "red";    
+ }
+ 
